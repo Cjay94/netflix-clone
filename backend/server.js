8ee3062691e67c62acf04ec30ceef170344e7b98
Cjay94 <cjephthah94@gmail.com>
2025-03-24 15:00:27 +0200

    Database (MongoDB) Setup

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 //const express = require('express'); //Commonjs
-import express from 'express'; //ESmodules
+import express from "express"; //ESmodules
 
 import authRoutes from "./routes/auth.route.js"
+import { ENV_VARS } from "./config/envVariables.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+const PORT = ENV_VARS.PORT
 
 app.use("/api/v1/auth", authRoutes);
 
 
-app.listen(5000, () => {
-    console.log('My server is alive at http://localhost:5000')
+app.listen(PORT, () => {
+    console.log(`My server is alive at http://localhost:${PORT}`)
+    connectDB();
 })
