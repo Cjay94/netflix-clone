bf9e4a85980e021fd31d0f97ee469d36b7423737
Cjay94 <cjephthah94@gmail.com>
2025-04-01 13:21:07 +0200

    Signup Page and Login Page UI Design + Added images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage"
+import SignUpPage from "./pages/home/SignUpPage"
+import LoginPage from "./pages/home/LoginPage"
+
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </>
   )
 }
 
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,6 +1,8 @@
 const HomePage = () => {
     return (
-        <div className="text-5xl text-center font-bold text-amber-300">welcome to the Home Page</div>
+        <div className="bg-hero h-screen">
+            welcome to the Home Page
+        </div>
     )
 }
 
